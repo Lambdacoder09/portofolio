f4eb764e3284d6993d077edf41e9eac5faf55f1e
zayedknight007 <zayedknight007@gmail.com>
2026-02-11 19:00:42 +0000

    started portfolio db

--- project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
-      © 2025 Zayed Khan. All Rights Reserved.
-    </footer>
-  );
-}
-
-export default Footer;
